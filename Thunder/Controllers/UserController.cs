efb9402ef18e0b6ba4597dec1569f622957800f4
Richard <richard.bohman@gmail.com>
2019-01-04 15:24:44 +0100

    Ändringar å så

--- Thunder/Controllers/UserController.cs
+++ Thunder/Controllers/UserController.cs
@@ -1,16 +1,35 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Thunder.Models;
 
 namespace Thunder.Controllers
 {
+
     public class UserController : Controller
     {
+        private ApplicationUser GetCurrentUser()
+        {
+            var userId = User.Identity.GetUserId();
+            var manager =
+                new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            var user = manager.FindById(userId);
+            return user;
+        }
+
         // GET: User
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult UserProfile()
+        {
+            var user = GetCurrentUser();
             return View();
         }
     }
--- Thunder/Migrations/201901041415185_Profile.cs
+++ Thunder/Migrations/201901041415185_Profile.cs
@@ -3,14 +3,16 @@ namespace Thunder.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class LastName : DbMigration
+    public partial class Profile : DbMigration
     {
         public override void Up()
         {
+            DropColumn("dbo.Profiles", "UserId");
         }
         
         public override void Down()
         {
+            AddColumn("dbo.Profiles", "UserId", c => c.String());
         }
     }
 }
