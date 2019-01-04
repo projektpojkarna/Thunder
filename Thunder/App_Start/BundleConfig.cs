dd2a823f98ee37761321bb0afb711cfa8d7ce862
Henrik Ojeda <henrik.ojeda@hotmail.com>
2019-01-04 16:27:23 +0100

    i

--- Thunder/App_Start/BundleConfig.cs
+++ Thunder/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Thunder
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap-nya.css",
+                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
