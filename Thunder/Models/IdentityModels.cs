8f8b7cce91f42070bbae099567a7ef148cd7f881
Richard <richard.bohman@gmail.com>
2019-01-04 12:45:18 +0100

    Test

--- Thunder/Models/IdentityModels.cs
+++ Thunder/Models/IdentityModels.cs
@@ -30,4 +30,24 @@ namespace Thunder.Models
             return new ApplicationDbContext();
         }
     }
+
+    public class MyUser : IdentityUser
+    {
+        public virtual MyUserInfo MyUserInfo { get; set; }
+    }
+
+    public class MyUserInfo
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    public class MyDbContext : IdentityDbContext<MyUser>
+    {
+        public MyDbContext()
+            : base("DefaultConnection")
+        {
+        }
+        public System.Data.Entity.DbSet<MyUserInfo> MyUserInfo { get; set; }
+    }
 }
