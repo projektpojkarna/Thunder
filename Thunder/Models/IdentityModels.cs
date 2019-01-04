0b31bbcf60e053d255ecf9c9531476806b8c0d19
Richard <richard.bohman@gmail.com>
2019-01-04 13:17:41 +0100

    FirstName och LastName tillagt i IdentityModel

--- Thunder/Models/IdentityModels.cs
+++ Thunder/Models/IdentityModels.cs
@@ -9,6 +9,9 @@ namespace Thunder.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -16,6 +19,7 @@ namespace Thunder.Models
             // Add custom user claims here
             return userIdentity;
         }
+
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
@@ -29,25 +33,5 @@ namespace Thunder.Models
         {
             return new ApplicationDbContext();
         }
-    }
-
-    public class MyUser : IdentityUser
-    {
-        public virtual MyUserInfo MyUserInfo { get; set; }
-    }
-
-    public class MyUserInfo
-    {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-    }
-    public class MyDbContext : IdentityDbContext<MyUser>
-    {
-        public MyDbContext()
-            : base("DefaultConnection")
-        {
-        }
-        public System.Data.Entity.DbSet<MyUserInfo> MyUserInfo { get; set; }
     }
 }
---
+++ Thunder/Models/Profile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Thunder.Models
+{
+    public class Profile
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+
+        //Presentation
+        //Intressen
+        //Stad
+        //Occupation: Jobb/studier
+        //Sökväg till bild
+    }
+}
