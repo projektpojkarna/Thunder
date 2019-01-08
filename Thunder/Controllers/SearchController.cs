ffe7fea2d7f74fd420dbdf26aae813b15dee647d
Henrik Ojeda <henrik.ojeda@hotmail.com>
2019-01-08 13:43:44 +0100

    Ändrat formulär samt lagt till controller

---
+++ Thunder/Controllers/SearchController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Thunder.Models;
+
+namespace Thunder.Controllers
+{
+    public class SearchController : Controller
+    {
+
+        private ProfileDbContext Db = new ProfileDbContext();
+
+        public ActionResult Index(string searchString)
+        {
+            return View(Db.Profiles.Where(s => s.FirstName == searchString || searchString == null).ToList());
+        }
+
+    }
+}
